bb51095dde62bff16e327c0e97d7584c3f8e694a
Luis Caceres <luiscaceresmx@gmail.com>
2023-11-14 09:04:34 +1100

    Update Board.js and unit tests

--- Board.js
+++ Board.js
@@ -1,9 +1,11 @@
 import { Grid } from "./Grid.js";
+import { List } from "./List.js";
 import { TreeNode } from "./TreeNode.js";
 
 class Board extends Grid {
 
-    #letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    static #alphabet = new List('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+    static #maximumNumberOfLettersInAWord = 9;
 
     /**
      * Return a list of tree data structures, each branch in a tree represents a path on this board. 
@@ -28,25 +30,25 @@ class Board extends Grid {
      */
     get paths() {
         // Let `paths` be an initially empty list of paths.
-        const trees = [];
+        const paths = [];
         // Let `squares` be a list of squares on this board.
         const squares = this.squares;
 
         // For each square `square` in `squares`.
         for (const square of squares) {
             // Let `tree` be an intially empty tree data structure with `square` as the root node.
-            const tree = new TreeNode(square);
+            const treeNode = new TreeNode(square);
 
-            (function recursion(path, square) {
+            (function recursion(treeNode, square) {
                 // Let `adjacentSquares` be `square`'s adjacent squares on this board.
                 const adjacentSquares = square.adjacentSquares.filter(square => square !== null);
 
                 // For each adjacent aquare `adjacentSquare` in `adjacentSquares`.
                 for (const adjacentSquare of adjacentSquares) {
                     // Let `sequence` be a sequence of continuous adjacent squares with `square` being the last square in the sequence.
-                    const sequence = path.ascendantNodes.map(node => node.value);
+                    const sequence = treeNode.ascendantNodes.map(node => node.value);
 
-                    // if `adjacentSquare` exists in `sequence`.
+                    // if `adjacentSquare` already exists in `sequence`.
                     if (sequence.includes(adjacentSquare)) {
                         // Skip `adjacentSquare` and proceed with the next adjacent square.
                         // NOTE: An infinite loop may occur without this conditional statement.
@@ -55,62 +57,63 @@ class Board extends Grid {
 
                     // Add `adjacentSquare` to `path`.
                     const childNode = new TreeNode(adjacentSquare);
-                    path.appendChildNodes(childNode);
+                    treeNode.appendChildNodes(childNode);
 
-                    recursion(childNode, adjacentSquare);
+                    if (childNode.ascendantNodes.length < Board.#maximumNumberOfLettersInAWord) {
+                        recursion(childNode, adjacentSquare);
+                    }
+                    else {
+                        // Add `path` to `paths`.
+                        paths.push(childNode.ascendantNodes);
+                    }
                 }
-            })(tree, square);
-
-            // Add `node` to `paths`.
-            trees.push(tree);
+            })(treeNode, square);
         }
 
-        return trees;
+        return paths;
     }
 
     /**
      * 
      */
     fill() {
-        // Let `paths` be a list of paths each representing...
-        let paths = this.paths.map(tree => tree.branches).flat();
-
+        // Let `paths` be a list of paths.
+        let paths = this.paths;
         // Let `blankSquares` be a list of blank squares in this board.
         const blankSquares = this.squares.filter(square => square.value === '');
 
         // While there are blank squares in this board.
-        while (blankSquares.length) {
-            // Let `lengths` be a list of the number of squares in each path in `paths`.
-            const lengths = paths.map(path => path.length);
-            // Let `maxLength` be the highest number in `lengths`.
-            const maxLength = Math.max(...lengths);
-            // Let `path` be the longest path in `paths`.
-            // What if there are no words with the same length as maxLengh?
-            const path = paths.find(path => path.length === maxLength);
-            // Let `squares` be a list of all the squares in `path`.
-            const squares = path.map(path => path.value);
+        while (blankSquares.length) {    
+            // Let `currentPath` be the longest path in `paths`.
+            const currentPath = new List(...paths).at(-1).sample();
+            // Let `squares` be a list of all the squares in `currentPath`.
+            const currentSquares = currentPath.map(path => path.value);
             // Let `letters` be an initially empty list of letters.
             const letters = [];
 
-            // If the number of squares in `squares` is less than or equal to 3.
-            if (squares.length <= 3) {
-                // Get random letters and add them to `letters`.
-                letters.push(...this.#letters.random(maxLength));
+            // If the number of squares in `currentPath` is less than or equal to 3.
+            if (currentPath.length <= 3) {
+                // Get the same number of letters from `alphabet` as the number of squares in `path`.
+                // Add those letters to `letters`.
+                letters.push(...Board.#alphabet.sample(maxLength));
             }
             else {
                 // Let `word` be a string whose number of letters doesn't exceed the number of squares in `path`.
                 // What if there are no words with the same length as maxLengh?
-                const word = this.words.filter(word => word.length === maxLength);
-                letters.push(word.split(''));
+                const words = this.words.filter(word => word.length === currentPath.length);
+                const word = words.sample();
+                letters.push(...word.split(''));
             }
 
             // For each square `square` in `squares`.
-            for (const square of squares) {
+            for (const square of currentSquares) {
                 // Associate `square` with a letter from `letters`.
                 square.value = letters.shift();
             }
-            
-            paths = paths.filter(path => squares.includes(path[0]) === false);
+
+            for (const paths of currentPath) {
+                currentPath.trim(square => currentSquares.includes(square));
+            }
         }
 
         
